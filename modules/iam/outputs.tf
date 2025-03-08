fc34c51ac36a148f7e62a8dc73b9890d91500fc9
Aditya Sonwane <aadus.info@gmail.com>
2025-03-08 13:47:25 +0530

    Delete modules directory

--- modules/iam/outputs.tf
+++
@@ -1,5 +0,0 @@
-# modules/iam/outputs.tf
-
-output "lambda_role_arn" {
-  value = aws_iam_role.lambda_role.arn
-}
--- modules/lambda/outputs.tf
+++
@@ -1,5 +0,0 @@
-# modules/lambda/outputs.tf
-
-output "lambda_function_arn" {
-  value = aws_lambda_function.crypto_lambda.arn
-}
